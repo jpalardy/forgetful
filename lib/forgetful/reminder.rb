3160b3dc9c4e44a07a8068983dd76e00854fb9ce
Jonathan Palardy <jonathan.palardy@gmail.com>
2011-08-01 12:20:48 -0400

    remove SuperMemo references from Reminder

--- lib/forgetful/reminder.rb
+++ lib/forgetful/reminder.rb
@@ -8,24 +8,7 @@ class Reminder
     @history = history.dup.freeze
   end
 
-  def ef
-    SuperMemo::traverse(history)[0]
-  end
-
-  def next(q)
-    new_history = history + [q]
-    Reminder.new(question, answer, SuperMemo::next_date(Date.today, new_history), new_history)
-  end
-
   def to_a
     [question, answer, due_on, history]
-  end
-
-  def <=>(other)
-    to_a <=> other.to_a
-  end
-
-  def review?
-    history.empty? || history.last < 4
   end
 end
--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -16,10 +16,6 @@ describe "reminder" do
     it "should be valid" do
       @reminder.to_a.should == [@question, @answer, Date.today, []]
     end
-
-    it "should have an ef of 2.5" do
-      @reminder.ef.should be_within(EPSILON).of(2.5)
-    end
   end
 
   describe "with full constructor" do
@@ -36,42 +32,6 @@ describe "reminder" do
       lambda {
         @reminder.history.push(5)
       }.should raise_error(error_type, "can't modify frozen array")
-    end
-
-    it "should have an ef of 2.8 (based on history)" do
-      @reminder.ef.should be_within(EPSILON).of(2.8)
-    end
-  end
-
-  describe "next" do
-    before do
-      @quality = 5
-      @reminder = Reminder.new(@question, @answer, @due_on, @history)
-    end
-
-    it "should create a new reminder scheduled appropriately" do
-      @reminder.next(@quality).to_a.should == [@question, @answer, Date.today+45, @history + [@quality]]
-    end
-  end
-
-  describe "review (w/o history)" do
-    subject { Reminder.new(@question, @answer) }
-
-    it { should be_review}
-  end
-
-  describe "review (w/ history)" do
-    before do
-      @reminder = Reminder.new(@question, @answer, @due_on, @history)
-    end
-
-    it "should qualify for review if quality < 4" do
-      @reminder.next(0).should be_review
-      @reminder.next(1).should be_review
-      @reminder.next(2).should be_review
-      @reminder.next(3).should be_review
-      @reminder.next(4).should_not be_review
-      @reminder.next(5).should_not be_review
     end
   end
 end
